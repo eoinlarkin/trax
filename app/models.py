e9db622b4fcb305b23394e1b721706536ab587db
eoinlarkin <eoin.larkin@gmail.com>
2022-05-26 00:09:45 +0100

    feat: create data model; allow raw file upload

--- app/models.py
+++ app/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.contrib.auth.models import User
+from cloudinary.models import CloudinaryField
 
 # Create your models here.
+
+class Activity(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    slug = models.SlugField(max_length=200, unique=True)
+    gpx_file = CloudinaryField('', default='manual', resource_type='raw')
+    date_created = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ["-date_created"]
+    
+    def __str__(self):
+        return self.slug
