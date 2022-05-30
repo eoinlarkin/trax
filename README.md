e78f5500df17a25cd4dffaa36a5d901bd70bc140
eoinlarkin <eoin.larkin@gmail.com>
2022-05-30 22:50:21 +0100

    refactor: move to gpxplotter for map plotting

--- README.md
+++ README.md
@@ -5,10 +5,12 @@
 https://hatarilabs.com/ih-en/developing-geospatial-webapps-with-python-and-django-tutorial
 
 # Overlay gpx on Folium Map:
-https://www.kaggle.com/code/paultimothymooney/overlay-gpx-route-on-osm-map-using-folium/notebook
+https://gpxplotter.readthedocs.io/en/latest/auto_examples/maps/plot_000_segment.html#sphx-glr-auto-examples-maps-plot-000-segment-py
+
 
 
 ## Cloudinary: Upload of non image files
 https://stackoverflow.com/questions/36805137/how-to-pass-options-to-cloudinaryfield-in-django-model
 
-
+## GPX - Plot Heartrate
+https://www.andersle.no/posts/2018/gpxplot/gpxplot.html
--- app/views.py
+++ app/views.py
@@ -1,13 +1,11 @@
 from django.shortcuts import render, redirect
 import os
-import folium
-
-from django.shortcuts import render, redirect
-import os
-import folium
-import gpxpy
+#import folium
 import urllib.request;
 
+from gpxplotter import read_gpx_file, create_folium_map, add_segment_to_map, plot_filled
+from matplotlib import pyplot as plt
+from io import StringIO
 
 # # Create your views here.
 # def home(request):
@@ -29,30 +27,39 @@ import urllib.request;
 
 # Create your views here.
 def home(request):
-    zoom = 14
-    #gpx_dir = os.path.join(os.getcwd(),'media','shp')
-    #gpx_file = open(os.path.join(gpx_dir, 'test.gpx'), 'r')
+    # Downloading the gpx file
     gpxPath = "https://res.cloudinary.com/dapgpdd7z/raw/upload/v1653520044/oubn2z8a8ws8wlc4e6s1.gpx"
-    #gpx_file = urllib.request.urlretrieve(gpxPath)
-
     urllib.request.urlretrieve(gpxPath, "temp.gpx")
 
-    #gpx_file = open(gpxData, 'r')
-    gpx_file = open(os.path.join(os.getcwd(), 'temp.gpx'), 'r')
-    gpx = gpxpy.parse(gpx_file)
-    points = []
-    for track in gpx.tracks:
-        for segment in track.segments:        
-            for point in segment.points:
-                points.append(tuple([point.latitude, point.longitude]))
-    latitude = sum(p[0] for p in points)/len(points)
-    longitude = sum(p[1] for p in points)/len(points)
-    myMap = folium.Map(location=[latitude,longitude],zoom_start=zoom)
-    folium.PolyLine(points, color="red", weight=2.5, opacity=1).add_to(myMap)
+    # Generating the Map Plot
+    # Define some properties for drawing the line:
+    line_options = {'color': 'blue', 'weight': 8, 'opacity': 0.5}
+    plt.style.use('seaborn-talk')
 
+    myMap = create_folium_map()
+    for track in read_gpx_file(os.path.join(os.getcwd(), 'temp.gpx')):
+        for i, segment in enumerate(track['segments']):
+            add_segment_to_map(myMap, segment, line_options=line_options)
+
+    def render_elev_plot():
+        for track in read_gpx_file(os.path.join(os.getcwd(), 'temp.gpx')):
+            for i, segment in enumerate(track['segments']):
+                plot_filled(track, segment, xvar='Distance / km', yvar='elevation',zvar='Velocity / km/h')
+        
+        imgdata = StringIO()
+        plt.savefig(imgdata, format='svg')
+        imgdata.seek(0)
+
+        data = imgdata.getvalue()
+        return data
+
+
+    #plt.savefig('matplot_test.png')
     ## exporting
     myMap=myMap._repr_html_()
-    context = {'my_map': myMap}
+    # elev_plot = render_elev_plot()._repr_html_()
+    #context = {'my_map': myMap}
+    context = {'my_map': myMap, 'elev_plot': render_elev_plot()}
     ## rendering
     return render(request,'home.html',context)
 
