75e41e3410a93360ae42131d249f9282736655b9
eoinlarkin <eoin.larkin@gmail.com>
2022-06-01 07:10:23 +0100

    feat: optimise zoom level for map

--- app/views.py
+++ app/views.py
@@ -3,10 +3,7 @@ from django.http import HttpResponseRedirect
 from .forms import ActivityForm
 from cloudinary.forms import cl_init_js_callbacks   
 
-import os
-import folium
-import gpxpy
-import urllib.request
+
 
 # # Create your views here.
 def home(request):
@@ -18,15 +15,16 @@ def about(request):
     return render(request,'about.html',context)
 
 def activity(request):
-    zoom = 14
-    #gpx_dir = os.path.join(os.getcwd(),'media','shp')
-    #gpx_file = open(os.path.join(gpx_dir, 'test.gpx'), 'r')
+    import os
+    import folium
+    import gpxpy
+    import urllib.request
+
+    #zoom = 14
     gpxPath = "https://res.cloudinary.com/dapgpdd7z/raw/upload/v1653520044/oubn2z8a8ws8wlc4e6s1.gpx"
-    #gpx_file = urllib.request.urlretrieve(gpxPath)
 
     urllib.request.urlretrieve(gpxPath, "temp.gpx")
 
-    #gpx_file = open(gpxData, 'r')
     gpx_file = open(os.path.join(os.getcwd(), 'temp.gpx'), 'r')
     gpx = gpxpy.parse(gpx_file)
     points = []
@@ -36,53 +34,20 @@ def activity(request):
                 points.append(tuple([point.latitude, point.longitude]))
     latitude = sum(p[0] for p in points)/len(points)
     longitude = sum(p[1] for p in points)/len(points)
-    myMap = folium.Map(location=[latitude,longitude],zoom_start=zoom)
+    myMap = folium.Map(location=[latitude,longitude]) 
     folium.PolyLine(points, color="red", weight=2.5, opacity=1).add_to(myMap)
+
+    # getting max and min lat and longitude to optimise zoom level
+    max_lat, max_lon = max(p[0] for p in points), max(p[1] for p in points)
+    min_lat, min_lon = min(p[0] for p in points), min(p[1] for p in points)   
+    myMap.fit_bounds([[min_lat, min_lon], [max_lat, max_lon], ])
 
     ## exporting
     myMap=myMap._repr_html_()
     context = {'my_map': myMap, 'elev_plot': myMap}
+
     ## rendering
     return render(request,'activity.html',context)
-
-# # Create your views here.
-# def home(request):
-#     # Downloading the gpx file
-#     gpxPath = "https://res.cloudinary.com/dapgpdd7z/raw/upload/v1653520044/oubn2z8a8ws8wlc4e6s1.gpx"
-#     urllib.request.urlretrieve(gpxPath, "temp.gpx")
-
-#     # Generating the Map Plot
-#     # Define some properties for drawing the line:
-#     line_options = {'color': 'blue', 'weight': 8, 'opacity': 0.5}
-#     plt.style.use('seaborn-talk')
-
-#     myMap = create_folium_map()
-#     for track in read_gpx_file(os.path.join(os.getcwd(), 'temp.gpx')):
-#         for i, segment in enumerate(track['segments']):
-#             add_segment_to_map(myMap, segment, line_options=line_options)
-
-#     def render_elev_plot():
-#         for track in read_gpx_file(os.path.join(os.getcwd(), 'temp.gpx')):
-#             for i, segment in enumerate(track['segments']):
-#                 plot_filled(track, segment, xvar='Distance / km', yvar='elevation',zvar='Velocity / km/h')
-        
-#         imgdata = StringIO()
-#         plt.savefig(imgdata, format='svg')
-#         imgdata.seek(0)
-
-#         data = imgdata.getvalue()
-#         return data
-
-
-#     #plt.savefig('matplot_test.png')
-#     ## exporting
-#     myMap=myMap._repr_html_()
-#     # elev_plot = render_elev_plot()._repr_html_()
-#     #context = {'my_map': myMap}
-#     context = {'my_map': myMap, 'elev_plot': myMap}
-#     ## rendering
-#     return render(request,'home.html',context)
-
 
 
 def AddActivity(request):
@@ -90,8 +55,6 @@ def AddActivity(request):
     # https://cloudinary.com/documentation/django_image_and_video_upload#django_forms_and_models
 
     context = dict( form = ActivityForm())
-
-
     if request.method == 'POST':
         form = ActivityForm(request.POST, request.FILES)
         context['posted'] = form.instance
@@ -101,5 +64,4 @@ def AddActivity(request):
             return HttpResponseRedirect("/") 
     else:
         form = ActivityForm()
-
     return render(request, "upload.html", context)
