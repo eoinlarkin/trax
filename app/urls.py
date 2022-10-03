eefef6992f5fba36db80eefd5bdb797d9fc05437
eoinlarkin <eoin.larkin@gmail.com>
2022-10-03 20:07:17 +0100

    chore: appending / to end of urls.py for app

--- app/urls.py
+++ app/urls.py
@@ -4,12 +4,12 @@ from . import views
 
 urlpatterns = [
     # path('', ActivityList.as_view(),name='home'),
-    path("", views.home, name="home"),
-    path("activity_feed", ActivityList.as_view(), name="activity_list"),
+    path("/", views.home, name="home"),
+    path("activity_feed/", ActivityList.as_view(), name="activity_list"),
     path("<slug:slug>/", ActivityDetail.as_view(), name="activity_detail"),
     path("like/<slug:slug>", ActivityLike.as_view(), name="activity_like"),
-    path("upload", views.add_activity, name="upload"),
-    path("about", views.about, name="about"),
+    path("upload/", views.add_activity, name="upload"),
+    path("about/", views.about, name="about"),
     path("edit/<slug:slug>/", views.edit, name="edit"),
     path("update/<slug:slug>/", update_activity, name="update"),
     path("delete/<slug:slug>/", views.ActivityDeleteView.as_view(), name="delete"),
